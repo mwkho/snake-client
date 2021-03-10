c7f1ef5f199f512c3cf38ccb5b2247293c121cda
mwkho <martin.h@hotmail.ca>
2021-03-10 20:56:46 +0000

    refactored play and client code so that connect is in the client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+let net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    port:50541,
+    host: 'localhost'
+  });
+
+conn.setEncoding('utf8');
+
+conn.on('data', (message) =>{
+  console.log(message);
+});
+
+return conn;
+}
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-let net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    port:50541,
-    host: 'localhost'
-  });
-
-conn.setEncoding('utf8');
-
-conn.on('data', (message) =>{
-  console.log(message);
-});
-
-return conn;
-}
+let connect = require('./client').connect
 
 console.log('connecting');
-connect()
+connect();
