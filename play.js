fdadc8d4a7001fdfef960a6e5872e405d5d60084
mwkho <martin.h@hotmail.ca>
2021-03-10 21:55:38 +0000

    added handleUserInput function process input from stdin

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 let connect = require('./client').connect
 
+const setupInput = function(){
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (input) => {
+    handleUserInput(input);
+  })
+  return stdin;
+
+}
+
+const handleUserInput = function(key){
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput();
 console.log('connecting');
 connect();
